2479c2ef4707e677922a7be2f7f90d28f70b166f
Kyle McAlpine <k@kylejm.io>
2015-07-20 16:21:34 +0100

    Initial Commit

---
+++ SuperPageControl/SuperPageControl.h
@@ -0,0 +1,19 @@
+//
+//  SuperPageControl.h
+//  SuperPageControl
+//
+//  Created by Kyle McAlpine on 20/07/2015.
+//  Copyright (c) 2015 Kyle McAlpine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SuperPageControl.
+FOUNDATION_EXPORT double SuperPageControlVersionNumber;
+
+//! Project version string for SuperPageControl.
+FOUNDATION_EXPORT const unsigned char SuperPageControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuperPageControl/PublicHeader.h>
+
+
