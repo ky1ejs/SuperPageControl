7e4c96f4129a72d39c6b63bf8b9b038c734a68c6
Kyle McAlpine <k@kylejm.io>
2015-07-20 22:04:54 +0100

    Development target created

---
+++ Development/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Development
+//
+//  Created by Kyle McAlpine on 20/07/2015.
+//  Copyright (c) 2015 Kyle McAlpine. All rights reserved.
+//
+
+import UIKit
+import SuperPageControl
+
+class ViewController: UIViewController {
+    @IBOutlet weak var pageControl: SuperPageControl!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
