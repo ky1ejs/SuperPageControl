64abaa40e7f304716e4c74fb4706d17f65b5d9a9
Kyle McAlpine <k@kylejm.io>
2016-07-06 22:07:08 +0100

    Added initialisation of SPC in code to Example

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -11,13 +11,32 @@ import SuperPageControl
 
 class ViewController: UIViewController {
     @IBOutlet weak var pageControl: SuperPageControl!
+    private var secondPageControl = SuperPageControl()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.pageControl.mode = DotMode.Individual({ (index, pageControl) -> DotMode in
-            self.pageControl.selectedDotSize = 20
+            pageControl.selectedDotSize = 20
             return .Shape(ShapeDotConfig(shape: .Circle))
         })
+        
+        self.secondPageControl.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(self.secondPageControl)
+        self.secondPageControl.centerXAnchor.constraintEqualToAnchor(self.view.centerXAnchor).active = true
+        self.secondPageControl.backgroundColor = .clearColor()
+        var shapeConfig = ShapeDotConfig(shape: .Circle)
+        shapeConfig.color = .redColor()
+        shapeConfig.setSelectedColor(.blueColor())
+        self.secondPageControl.mode = .Shape(shapeConfig)
+        let topConstraint = self.secondPageControl.topAnchor.constraintEqualToAnchor(self.view.topAnchor)
+        topConstraint.constant = 100
+        topConstraint.active = true
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.secondPageControl.numberOfPages = 5
+        self.secondPageControl.currentPage = 2
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,10 +46,12 @@ class ViewController: UIViewController {
     
     @IBAction func next(sender: AnyObject) {
         self.pageControl.currentPage += 1
+        self.secondPageControl.currentPage += 1
     }
     
     @IBAction func previous(sender: AnyObject) {
         self.pageControl.currentPage -= 1
+        self.secondPageControl.currentPage -= 1
     }
 }
 
