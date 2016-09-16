200891f195b7a3a64a8f3dc146df6ef2cc1faf37
Kyle McAlpine <k@kylejm.io>
2016-09-16 12:15:56 +0100

    Upgraded to Swift 2.3

--- SuperPageControl/UIImageHelpers.swift
+++ SuperPageControl/UIImageHelpers.swift
@@ -11,16 +11,16 @@ import UIKit
 extension UIImage {
     func tintWithColor(color: UIColor) -> UIImage {
         UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
-        let context = UIGraphicsGetCurrentContext()
+        let context = UIGraphicsGetCurrentContext()!
         CGContextSaveGState(context)
         CGContextTranslateCTM(context, 0, self.size.height)
         CGContextScaleCTM(context, 1.0, -1.0)
         CGContextSetBlendMode(context, CGBlendMode.Normal)
         let rect = CGRectMake(0, 0, self.size.width, self.size.height)
-        CGContextClipToMask(context, rect, self.CGImage)
+        CGContextClipToMask(context, rect, self.CGImage!)
         color.setFill()
         CGContextFillRect(context, rect)
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
         CGContextRestoreGState(context)
         return newImage
